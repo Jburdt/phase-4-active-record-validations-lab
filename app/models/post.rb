ce2640e553c845e42ad816b7a4674c3e67f514d3
Josh Burd <Jburdt@gmail.com>
2023-02-09 11:46:58 -0500

    commit tree 8dacdc49b34ad29d4ea18eafc616bbd2e51df053

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :title_not_to 
+
+  def title_not_to
+    if title === "True Facts"
+    errors.add(:title, "True Facts")
+    end
+  end
+
 end
